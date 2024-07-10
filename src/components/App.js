5dbc7e13ceb363edf9b47b2a173baf55abbce814
Amruthraj-coder <122180037+Amruthraj-coder@users.noreply.github.com>
2024-07-10 12:57:45 +0000

    initail

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
+import React, { useState } from "react";
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+const Element = ({ value, id, handleDelete }) => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <span>{value}</span>
+      <button onClick={() => handleDelete(id)}>delete</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const Todo = () => {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [id, setId] = useState(0);
+
+  const handleAddTodo = () => {
+    if (value.trim() !== "") {
+      const newTodo = { value, id };
+      setTodos([...todos, newTodo]);
+      setId(id + 1);
+      setValue(""); // Clear the input field
+    }
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={handleAddTodo}>Add To-do</button>
+      <div>
+        {todos.map((todo) => (
+          <Element
+            key={todo.id}
+            value={todo.value}
+            id={todo.id}
+            handleDelete={handleDeleteTodo}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
